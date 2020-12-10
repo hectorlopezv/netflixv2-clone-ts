c5d52bf2138a7d0404b502378a9cdd5945bfdf1e
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-12-10 10:05:38 -0500

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, combineReducers } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const rootReducer = combineReducers({
+
+});
+
+
+const composeEnchancers = composeWithDevTools({
+  trace: true,
+  traceLimit: 25
+});
+
+const store = createStore(() => {}, composeEnchancers(
+  applyMiddleware(sagaMiddleware),
+));
+//run saga listeners
+//sagaMiddleware.run();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Provider store={store}>
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
