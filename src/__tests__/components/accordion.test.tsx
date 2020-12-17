6850d2c5cb5eda19048e383c9b1056b045b4510a
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-12-17 11:44:38 -0500

    adding testing for some components using react testing library

--- src/__tests__/components/accordion.test.tsx
+++ src/__tests__/components/accordion.test.tsx
@@ -13,6 +13,48 @@ describe('Testing Accordion', () => {
 
     it('test <Accordion /> with populated data', () => {
 
+        const {container, getByText} = render(
+            <Accordion>
+            <Accordion_Title>Frequently Asked Questions</Accordion_Title>
+            {faqsData.map((item: any) => {
+                return <Accordion_Item key={item.id}>
+                    <Accordion_Header>{item.header}</Accordion_Header>
+                    <Accordion_Body>{item.body}</Accordion_Body>
+                </Accordion_Item>
+            })}
+        </Accordion>
+        );
 
+        expect(getByText('What is Netflix?')).toBeTruthy();
+        expect(getByText('How much does Netflix cost?')).toBeTruthy();
+        expect(getByText('Where can I watch?')).toBeTruthy();
+        expect(getByText('How do I cancel?')).toBeTruthy();
+        expect(getByText('What can I watch on Netflix?')).toBeTruthy();
+    });
+
+    it('opens and closes the accordion <Accordion /> Component', () => {
+        const {container, getByText, queryByText} = render(
+            <Accordion>
+            <Accordion_Title>Frequently Asked Questions</Accordion_Title>
+            {faqsData.map((item: any) => {
+                return <Accordion_Item key={item.id}>
+                    <Accordion_Header>{item.header}</Accordion_Header>
+                    <Accordion_Body>{item.body}</Accordion_Body>
+                </Accordion_Item>
+            })}
+        </Accordion>
+        );
+        const text = "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+        
+        expect(queryByText(text)).toBeFalsy();
+
+        // //we expect now to be true when we click it
+        // fireEvent.click(queryByText("What is Netflix?")!);
+        // expect(queryByText(text)).toBeTruthy();
+
+        // fireEvent.click(queryByText("What is Netflix?")!);
+        // expect(queryByText(text)).toBeFalsy();
+        expect(container.firstChild).toMatchSnapshot();
+    
     });
 });
---
+++ src/__tests__/components/card.test.tsx
@@ -0,0 +1,147 @@
+/* eslint-disable react/jsx-pascal-case */
+import React from 'react';
+import { render, fireEvent, getByTestId, getByAltText } from '@testing-library/react';
+import Card, 
+{Card_Text,
+    Card_Feature,
+    Card_Group, Card_Title, Card_Entities, Card_Item, Card_Image, Card_Meta, Card_SubTitle
+} from '../../components/Card';
+
+import Player, 
+{
+    Player_Button, Player_Video
+} from '../../components/Player';
+
+
+
+const category = 'series';
+const slideRows = [
+  {
+    title: 'Documentaries',
+    data: [
+      {
+        genre: 'documentaries',
+        maturity: '18',
+        slug: 'tiger-king',
+        description: 'Tiger King description',
+        id: '07adcf19-517f-41b4-8f8b-04fee694bce1',
+        title: 'Tiger King',
+        docId: 'IcRxJBanzE7nmIp2GMGb',
+      },
+    ],
+  },
+  {
+    title: 'Feel Good',
+    data: [
+      {
+        title: 'Juno',
+        genre: 'feel-good',
+        description: 'Juno description',
+        maturity: '0',
+        id: 'a938b5a6-fe25-4a06-8050-353bc7146ccd',
+        slug: 'juno',
+        docId: '4JDgdf5vE0K3YrW9ZnbP',
+      },
+    ],
+  },
+];
+
+
+describe('<Card /> Component', () => {
+
+    it('<Card > Renders the card with populated data', () => {
+        const {container, getByText, queryByText} =  render (
+            <Card_Group>
+                {slideRows.map((slideItem: any) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>{/* carc container*/}
+                        <Card_Title>{slideItem.title}</Card_Title>
+                        <Card_Entities>{/*Card Carrousel*/}
+                            {/* Card Item*/}
+                            {slideItem.data.map((item: any) => ( 
+                                <Card_Item key={item.docId} item={item}>
+                                    <Card_Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                                    <Card_Meta>
+                                        <Card_SubTitle>{item.title}</Card_SubTitle>
+                                        <Card_Text>{item.description}</Card_Text>
+                                    </Card_Meta>
+                                </Card_Item>
+                            ))}
+                        </Card_Entities>
+                        <Card_Feature category={category}>
+                            <Player>
+                                <Player_Button />
+                                <Player_Video src={"/videos/bunny.mp4"}/>
+                            </Player>
+                        </Card_Feature>
+
+                    </Card> 
+                ))}
+            </Card_Group>
+        );
+
+        //asertions
+        expect(getByText('Documentaries')).toBeTruthy();
+        expect(getByText('Tiger King')).toBeTruthy();
+        expect(getByText('Tiger King description')).toBeTruthy();
+
+        expect(getByText('Feel Good')).toBeTruthy();
+        expect(getByText('Juno')).toBeTruthy();
+        expect(getByText('Juno description')).toBeTruthy();
+
+        expect(container.firstChild).toMatchSnapshot();
+
+
+    });
+
+
+    it('renders <Card > testing toogling  setFeature and showFeature', () => {
+        const {container, getByText, queryByText, getByAltText, queryByTestId, getByTestId} =  render (
+            <Card_Group>
+                {slideRows.map((slideItem: any) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>{/* carc container*/}
+                        <Card_Title>{slideItem.title}</Card_Title>
+                        <Card_Entities>{/*Card Carrousel*/}
+                            {/* Card Item*/}
+                            {slideItem.data.map((item: any) => ( 
+                                <Card_Item key={item.docId} item={item} data-testid={`${item.slug}-item-feature`}>
+                                    <Card_Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                                    <Card_Meta>
+                                        <Card_SubTitle>{item.title}</Card_SubTitle>
+                                        <Card_Text>{item.description}</Card_Text>
+                                    </Card_Meta>
+                                </Card_Item>
+                            ))}
+                        </Card_Entities>
+                        <Card_Feature category={category}>
+                            <Player>
+                                <Player_Button />
+                                <Player_Video src={"/videos/bunny.mp4"}/>
+                            </Player>
+                        </Card_Feature>
+
+                    </Card> 
+                ))}
+            </Card_Group>
+        );
+
+        //asertions
+        expect(queryByText('18')).toBeFalsy();
+        fireEvent.click(getByTestId('tiger-king-item-feature')!);
+        expect(queryByText('18')).toBeTruthy();
+
+        //click close button
+        fireEvent.click(getByAltText('Close')!);
+        expect(queryByText('18')).toBeFalsy();
+
+        expect(queryByText('PG')).toBeFalsy();
+        
+        fireEvent.click(getByTestId('juno-item-feature')!);
+        expect(queryByText('PG')).toBeTruthy();
+
+        fireEvent.click(getByAltText('Close')!);
+        expect(queryByText('PG')).toBeFalsy();
+
+        expect(container.firstChild).toMatchSnapshot();
+        
+    });
+});
